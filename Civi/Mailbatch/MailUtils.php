c6073d3c40b6f097c0322a9c739752ba81642b39
Jens Schuppe <schuppe@systopia.de>
2025-06-23 13:33:07 +0200

    Code style

--- Civi/Mailbatch/MailUtils.php
+++ Civi/Mailbatch/MailUtils.php
@@ -6,11 +6,13 @@ class MailUtils {
 
   /**
    * Get a list of the available/allowed sender email addresses
+   *
+   * @phpstan-return array<int, string>
    */
-  public static function getSenderOptions($asHtml = TRUE) {
+  public static function getSenderOptions(bool $asHtml = TRUE): array {
     // TODO: Remove check when minimum core version requirement is >= 6.0.0.
     if (class_exists('\Civi\Api4\SiteEmailAddress')) {
-      $from_email_addresses = \Civi\Api4\SiteEmailAddress::get(FALSE)
+      $fromEmailAddresses = \Civi\Api4\SiteEmailAddress::get(FALSE)
         ->addSelect('display_name', 'email', 'id')
         ->addWhere('domain_id', '=', 'current_domain')
         ->addWhere('is_active', '=', TRUE)
@@ -19,17 +21,17 @@ class MailUtils {
         ->indexBy('id')
         ->getArrayCopy();
       // Include "email" column as the option value label did.
-      $from_email_addresses = array_map(
+      $fromEmailAddresses = array_map(
         fn($address) => sprintf(
           '"%s" <%s>',
           $address['display_name'],
           $address['email']
         ),
-        $from_email_addresses
+        $fromEmailAddresses
       );
     }
     else {
-      $from_email_addresses = OptionValue::get(FALSE)
+      $fromEmailAddresses = OptionValue::get(FALSE)
         ->addSelect('value', 'label')
         ->addWhere('domain_id', '=', 'current_domain')
         ->addWhere('option_group_id:name', '=', 'from_email_address')
@@ -41,8 +43,8 @@ class MailUtils {
     }
 
     return $asHtml
-      ? array_map(fn($label) => htmlspecialchars($label), $from_email_addresses)
-      : $from_email_addresses;
+      ? array_map(fn($label) => htmlspecialchars($label), $fromEmailAddresses)
+      : $fromEmailAddresses;
   }
 
 }
